972cb0b38c2f00ede750cab1bffe7c3b5e42828b
DanCRk <dancrkyt@gmail.com>
2023-01-19 18:16:47 -0600

    CONFIGURAR A NIVEL DE APLICACION COGNITO Y STORAGE CON LA TERMINAL

--- app/src/main/java/com/dancrk/awsbeta/AWSBeta.kt
+++ app/src/main/java/com/dancrk/awsbeta/AWSBeta.kt
@@ -3,14 +3,17 @@ package com.dancrk.awsbeta
 import android.app.Application
 import android.util.Log
 import com.amplifyframework.AmplifyException
-import com.amplifyframework.core.Amplify
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.kotlin.core.Amplify
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin
 
 class AWSBeta : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
         try {
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+            Amplify.addPlugin(AWSS3StoragePlugin())
             Amplify.configure(applicationContext)
             Log.i("MyAmplifyApp", "Initialized Amplify")
         } catch (error: AmplifyException) {
